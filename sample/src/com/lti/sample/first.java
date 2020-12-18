c69db6b86f1a6ef5e4f132adccad097b625135d9
Padmaraj <padmaraj0292@gmail.com>
2020-12-18 10:15:42 +0530

    second change

--- sample/src/com/lti/sample/first.java
+++ sample/src/com/lti/sample/first.java
@@ -5,6 +5,9 @@ public class first {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+int a=10,b=20;
+int c=a+b;
+System.out.println(c);
 	}
 
 }
