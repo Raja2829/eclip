26a58da96b18eb029e453538dcf46f6277dcbed2
Padmaraj <padmaraj0292@gmail.com>
2020-12-18 12:27:27 +0530

    added op stmt

--- sample/src/com/lti/sample/first.java
+++ sample/src/com/lti/sample/first.java
@@ -3,11 +3,13 @@ package com.lti.sample;
 public class first {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 System.out.println("hello");
+
 int a=10,b=20;
 int c=a+b;
 System.out.println(c);
+System.out.println("hi");
 	}
 
 }
